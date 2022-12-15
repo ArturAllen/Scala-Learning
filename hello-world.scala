3013ea085e8d4a3b458bef4b781024a4893d31da
Artur <72237233+ArturAllen@users.noreply.github.com>
2022-12-15 11:02:25 -0300

    Update hello-world.scala

--- hello-world.scala
+++ hello-world.scala
@@ -1,8 +1,5 @@
 object HelloWorld {
   def main(args: Array[String]) : Unit = {
-    println("Hello, world!")
-
-    val name = "Artur"
-    println(s"Hello, ${name}!")
+    println("Hello, world!"
   }
 }
