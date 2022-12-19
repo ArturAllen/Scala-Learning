827129ae190b6cc770a2d874fb89a70326bd385d
Artur <72237233+ArturAllen@users.noreply.github.com>
2022-12-19 17:18:27 -0300

    Create Flow-Control.scala

---
+++ Flow-Control.scala
@@ -0,0 +1,14 @@
+object FlowControl {
+  def main(args: Array[String]): Unit = {
+    val name = "Jose"
+    if (name == "Frank") {
+      println("Welcome Frank")
+    }
+    else if (name == "Sammy") {
+      println("Welcome Sammy")
+    }
+    else {
+      println("What is your name")
+    }
+  }
+}
